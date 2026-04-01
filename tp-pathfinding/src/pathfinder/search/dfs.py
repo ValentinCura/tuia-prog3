395afa9d2dbaefbc62dc9623ae1485562244ac02
Valentin Cura <valencuran@gmail.com>
2026-04-01 19:52:18 -0300

    Feat: DFS creation

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -25,4 +25,21 @@ class DepthFirstSearch:
         # TODO Complete the rest!!
         # ...
 
-        return NoSolution(expanded)
+        frontier = StackFrontier()
+        frontier.add(root)
+
+        while True:
+            if frontier.is_empty():
+                return NoSolution(expanded)
+            node = frontier.remove()
+            if node.state in expanded:
+                continue
+            expanded[node.state] = True
+            for action in grid.actions(node.state):
+                node_after_action = grid.result(node.state,action)
+                if node_after_action not in expanded:
+                    node2 = Node("",state=node_after_action,parent=node,action=action,cost=node.cost + grid.individual_cost(node.state,action))
+                    if grid.objective_test(node2.state):
+                        return Solution(node2,reached=expanded)
+                    frontier.add(node2)
+
