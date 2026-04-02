8d5ab7e5b2d72a4b93731a87f0713f95498f2938
Sebastián Beltramo <blackdeins@gmail.com>
2026-04-02 00:36:17 -0300

    Feat: BFS creation

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -3,17 +3,16 @@ from ..models.frontier import QueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
-
 class BreadthFirstSearch:
     @staticmethod
     def search(grid: Grid) -> Solution:
         """Find path between two points in a grid using Breadth First Search
 
         Args:
-            grid (Grid): Grid of points
+        grid (Grid): Grid of points
 
         Returns:
-            Solution: Solution found
+        Solution: Solution found
         """
         # Initialize root node
         root = Node("", state=grid.initial, cost=0, parent=None, action=None)
@@ -22,8 +21,26 @@ class BreadthFirstSearch:
         reached = {}
         reached[root.state] = True
 
+        if grid.objective_test(root.state):
+            return Solution(root, reached)
+
         # Initialize frontier with the root node
+        frontier = QueueFrontier()
+        frontier.add(root)
+
         # TODO Complete the rest!!
         # ...
+        while True:
+            if frontier.is_empty(): return False
+            n = frontier.remove()
+            for a in grid.actions(n.state):
+                if a not in grid.actions(n.state): continue
+                s = grid.result(n.state, a)
+                if s in reached: continue
+                else:
+                    np = Node("", s, n.cost + grid.individual_cost(n.state, a), n, a)
+                    if grid.objective_test(s): return Solution(np, reached)
+                    reached[s] = True
+                    frontier.add(np)
 
         return NoSolution(reached)
